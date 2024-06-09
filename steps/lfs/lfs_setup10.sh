abafb2fec2748b6cad212825cf92cf047c932cb1
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-06-09 12:11:53 -0400

    fix typo in NSS

--- steps/lfs/lfs_setup10.sh
+++ steps/lfs/lfs_setup10.sh
@@ -155,7 +155,7 @@ tar -xvf nss-3.92.tar.gz
 cd nss-3.92
 patch -Np1 -i ../nss-3.92-standalone-1.patch
 cd nss
-make BUILD_OPT=1  NSPR_INCLUDE_DIR=/usr/include/nspr USE_SYSTEM_ZLIB=1 ZLIB_LIBS=-lz NSS_ENABLE_WERROR=0 /
+make BUILD_OPT=1  NSPR_INCLUDE_DIR=/usr/include/nspr USE_SYSTEM_ZLIB=1 ZLIB_LIBS=-lz NSS_ENABLE_WERROR=0 \
 $([ $(uname -m) = x86_64 ] && echo USE_64=1) $([ -f /usr/include/sqlite3.h ] && echo NSS_USE_SYSTEM_SQLITE=1)
 cd ../dist   
 install -v -m755 Linux*/lib/*.so              /usr/lib 
