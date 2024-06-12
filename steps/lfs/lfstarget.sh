271bec49032d56cba1ec4e5d038ff3a35b606083
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-06-11 23:10:14 -0400

    update to move files into the target directory

--- steps/lfs/lfstarget.sh
+++ steps/lfs/lfstarget.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
 git clone https://github.com/ajherchenroder/live-bootstrap-distro-build-scripts.git
+mkdir target
+cp /live-bootstrap-distro-build-scripts/* /target
+rm -Rf /live-bootstrap-distro-build-scripts
+
 echo "cd into /target and run the desired build script"
  
