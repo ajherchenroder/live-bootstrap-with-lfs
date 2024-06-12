bbf154e252088b93a51edab543c9ba6521d61a68
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-06-11 23:17:36 -0400

    fix copy command for the target script

--- steps/lfs/lfstarget.sh
+++ steps/lfs/lfstarget.sh
@@ -2,7 +2,7 @@
 set -e
 git clone https://github.com/ajherchenroder/live-bootstrap-distro-build-scripts.git
 mkdir target
-cp /live-bootstrap-distro-build-scripts/* /target
+cp /live-bootstrap-distro-build-scripts/target/* /target
 rm -Rf /live-bootstrap-distro-build-scripts
 
 echo "cd into /target and run the desired build script"
