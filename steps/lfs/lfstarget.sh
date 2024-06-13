25d1995cf6d2cecb454430e5948f0255551e0d2e
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-06-13 12:54:34 -0400

    make target scripts runnable

--- steps/lfs/lfstarget.sh
+++ steps/lfs/lfstarget.sh
@@ -4,6 +4,6 @@ git clone https://github.com/ajherchenroder/live-bootstrap-distro-build-scripts.
 mkdir target
 cp /live-bootstrap-distro-build-scripts/target/* /target
 rm -Rf /live-bootstrap-distro-build-scripts
-
+chmod +x,+x,+x /target/*
 echo "cd into /target and run the desired build script"
  
