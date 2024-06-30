0fc28d6da1004281f4c727b1e2f8e3c6ab116b84
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-06-30 02:53:02 -0400

    second attempt to fix bzip2

--- steps/lfs/lfs_setup7.sh
+++ steps/lfs/lfs_setup7.sh
@@ -148,7 +148,7 @@ make
 make PREFIX=/usr install
 cp -av libbz2.so.* /usr/lib
 ln -sv libbz2.so.1.0.8 /usr/lib/libbz2.so
-cp -v bzip2-shared /usr/bin/bzip2
+cp -v /sources/bzip2-1.0.8/bzip2-shared /usr/bin/bzip2
 ln -sfv bzip2 /usr/bin/bzcat
 ln -sfv bzip2 /usr/bin/bunzip2
 rm -fv /usr/lib/libbz2.a
