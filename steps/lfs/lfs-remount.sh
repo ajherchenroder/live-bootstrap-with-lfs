649e9723e241bc92fc104e99d4c6d03721644bed
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-07-09 18:49:22 -0400

    Update lfs-remount.sh to mount sysfs

--- steps/lfs/lfs-remount.sh
+++ steps/lfs/lfs-remount.sh
@@ -5,6 +5,6 @@ echo "remounting LFS file systems"
 mount -vt devtmpfs devtmpfs /dev
 mount -vt devpts devpts /dev/pts
 mount -vt proc proc /proc
-#mount -vt sysfs sysfs /sys
+mount -vt sysfs sysfs /sys
 mount -vt tmpfs tmpfs /run
 
