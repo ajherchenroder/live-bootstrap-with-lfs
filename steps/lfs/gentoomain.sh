f8c858d8b128ecfbb2f3180304cd267499021b9b
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2024-09-10 01:45:13 -0700

    split Gentoo script into multiple to support a chroot

---
+++ steps/lfs/gentoomain.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# based on gentoo.txt located at https://mid-kid.root.sx/git/mid-kid/bootstrap in the gentoo-2024 folder
+# 
+#MIT License
+#
+#Copyright (c) 2017 Braintree
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+cp gentoomk.sh /
+cp gentoomk2.sh /
+./gentoomk.sh
+chroot /gentoo /bin/bash --login gentoomk2.sh
+
+
--- steps/lfs/gentoomk.sh
+++ steps/lfs/gentoomk.sh
@@ -14,9 +14,9 @@
 #
 #The above copyright notice and this permission notice shall be included in all
 #copies or substantial portions of the Software.
-mount -t proc proc proc
-mount -t sysfs sysfs sys
-mount -t devtmpfs devtmpfs dev
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -t devtmpfs devtmpfs /dev
 mount -t devpts devpts dev/pts
 mount -t tmpfs -o nosuid,nodev tmpfs /dev/shm 
 source /steps/env
@@ -287,41 +287,7 @@ echo 'C.UTF8 UTF-8' > /gentoo/etc/locale.gen
 # Copy ::gentoo repo and distfiles
 rsync -aP /var/db/repos/ /gentoo/var/db/repos
 rsync -aP /var/cache/distfiles/ /gentoo/var/cache/distfiles
-chroot /gentoo
-mount -t proc proc proc
-mount -t sysfs sysfs sys
-mount -t devtmpfs devtmpfs dev
-mount -t devpts devpts dev/pts
-mount -t tmpfs -o nosuid,nodev tmpfs /dev/shm 
 
-emerge -O1n \
-    app-alternatives/awk \
-    app-alternatives/bzip2 \
-    app-alternatives/gzip \
-    app-alternatives/lex \
-    app-alternatives/ninja \
-    app-alternatives/tar \
-    app-alternatives/yacc
-
-# Finish installing stage1 dependencies
-pkgs_build="$(python3 -c 'import portage
-print(*portage.util.stack_lists([portage.util.grabfile_package("%s/packages.build" % x) for x in portage.settings.profiles], incremental=1))')"
-USE="-* build $(portageq envvar BOOTSTRAP_USE)" CHOST="$(gcc -dumpmachine)" \
-    emerge -1Dn $pkgs_build
-emerge -c  # Make sure the dependency tree is consistent
-
-# Change CHOST and build OpenMP support (stage2-ish)
-emerge -1 sys-devel/binutils
-emerge -o sys-devel/gcc
-EXTRA_ECONF=--disable-bootstrap emerge -O1 sys-devel/gcc
-emerge -1 $(portageq expand_virtual / virtual/libc)
-emerge -1 dev-lang/perl  # https://bugs.gentoo.org/937918
-
-# Rebuild everything (stage3)
-USE='-filecaps -http2' emerge -e @system
-USE='-filecaps -http2' emerge  @system
-emerge -DN @system
-emerge -c
 
 
 
