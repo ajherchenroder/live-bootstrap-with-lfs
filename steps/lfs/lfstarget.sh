cc90e3a90740a506331fd422a4437959d2a5ceff
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2025-02-28 12:06:55 -0500

    update lfstarget to make it easier to initiate a Gentoo bootstrap

--- steps/lfs/lfstarget.sh
+++ steps/lfs/lfstarget.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -e
+DISKTOUSE=$(</lfsdisktouse)
+mount $DISKTOUSE'2' /mnt
 git clone https://github.com/ajherchenroder/live-bootstrap-distro-build-scripts.git
 mkdir target
 cp /live-bootstrap-distro-build-scripts/target/* /target
+mkdir /mnt/live-bootstrap/target/target
+cp /live-bootstrap-distro-build-scripts/target/* /mnt/live-bootstrap/target/target/target
 rm -Rf /live-bootstrap-distro-build-scripts
 chmod +x,+x,+x /target/*
+chmod +x,+x,+x /mnt/live-bootstrap/target/target/*
 echo "cd into /target and run the desired build script"
- 
+echo "for Gentoo return to the live-bootstrap and then cd into /target"
+umount /mnt
