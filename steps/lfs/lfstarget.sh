e68b4f31a18c850d2f07c44edb64edb4afb3bfa1
ajherchenroder <136374399+ajherchenroder@users.noreply.github.com>
2025-10-31 02:27:46 -0400

    remove note on needing to go back to the livebootstrap for Gentoo

--- steps/lfs/lfstarget.sh
+++ steps/lfs/lfstarget.sh
@@ -11,5 +11,5 @@ rm -Rf /live-bootstrap-distro-build-scripts
 chmod +x,+x,+x /target/*
 chmod +x,+x,+x /mnt/live-bootstrap/target/target/*
 echo "cd into /target and run the desired build script"
-echo "for Gentoo return to the live-bootstrap and then cd into /target"
+#echo "for Gentoo return to the live-bootstrap and then cd into /target"
 umount /mnt
